27d2530c4610f49aaa372120a6df2654088ca68a
benhenshaw <benedicthenshaw@gmail.com>
2018-06-08 21:55:32 +0100

    Added basic framerate lock

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #define assert SDL_assert
 
 typedef uint32_t u32;
+typedef uint64_t u64;
+typedef float f32;
 
 int main()
 {
@@ -29,8 +31,15 @@ int main()
         &audio_spec_request, NULL, 0);
     assert(audio_device);
 
+    u64 target_frame_rate = 60;
+    u64 ticks_per_second = SDL_GetPerformanceFrequency();
+    u64 ticks_per_frame = ticks_per_second / target_frame_rate;
+    u64 ticks_per_millisecond = ticks_per_second / 1000;
+
     while (true)
     {
+        u64 time_stamp = SDL_GetPerformanceCounter();
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -38,6 +47,12 @@ int main()
             {
                 exit(0);
             }
+        }
+
+        while (SDL_GetPerformanceCounter() - time_stamp <
+                ticks_per_frame - ticks_per_millisecond)
+        {
+            SDL_Delay(1);
         }
 
         SDL_UpdateWindowSurface(window);
